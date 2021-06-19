d6eaa357938524096d4776cab77e9f1fbc7b56f8
RendelFlegg <62382457+RendelFlegg@users.noreply.github.com>
2021-06-19 15:24:01 +0500

    Stage 4 => Stage 5
    Add functionality to admit applicants to different departments by results of their exams instead of GPA

--- python_developer/medium/university.py
+++ python_developer/medium/university.py
@@ -3,19 +3,27 @@ students = []
 stages = ["plan_a", "plan_b", "plan_c"]
 departments = {"Biotech", "Chemistry", "Engineering", "Mathematics", "Physics"}
 departments_dict = {"Biotech": [], "Chemistry": [], "Engineering": [], "Mathematics": [], "Physics": []}
+exams = {"Biotech": "chem_exam", "Chemistry": "chem_exam", "Engineering": "com_science_exam",
+         "Mathematics": "math_exam", "Physics": "phys_exam"}
 with open("applicants.txt", "r") as f:
-    raw_results = [applicant.rsplit(" ", 4) for applicant in f.read().splitlines()]
+    raw_results = [applicant.rsplit(" ", 7) for applicant in f.read().splitlines()]
 results = {}
+list_of_keys = ["phys_exam", "chem_exam", "math_exam", "com_science_exam", "plan_a", "plan_b", "plan_c"]
 for result in raw_results:
-    results[result[0]] = {"gpa": result[1], "plan_a": result[2], "plan_b": result[3], "plan_c": result[4]}
+    results[result[0]] = dict(zip(list_of_keys, result[1:]))
 
 
 def department_sorting(department, applicants, stage):
+    places = number_of_students - len(departments_dict[department])
+    temp_departments_dict = {"Biotech": [], "Chemistry": [], "Engineering": [], "Mathematics": [], "Physics": []}
     for applicant in applicants:
         if department in (results[applicant][stage]):
-            departments_dict[department].append((applicant, results[applicant]["gpa"]))
-            departments_dict[department].sort(key=lambda x: (-float(x[1]), x[0]))
-            departments_dict[department] = departments_dict[department][:number_of_students]
+            temp_departments_dict[department].append((applicant, results[applicant][exams[results[applicant][stage]]]))
+    temp_departments_dict[department].sort(key=lambda x: (-float(x[1]), x[0]))
+    temp_departments_dict[department] = temp_departments_dict[department][:places]
+    for student in temp_departments_dict[department]:
+        departments_dict[department].append(student)
+    departments_dict[department].sort(key=lambda x: (-float(x[1]), x[0]))
 
 
 def rotate():
@@ -37,5 +45,6 @@ while len(departments) != 0 and len(students) != len(results):
 for item in departments_dict.items():
     print(item[0])
     for result in item[1]:
-        print(f"{result[0]} {result[1]}")
+        print(f"{result[0]} {float(result[1])}")
     print("")
+
