57d629524658a461e530c0e344227de649e59ef9
RendelFlegg <62382457+RendelFlegg@users.noreply.github.com>
2021-06-19 15:31:39 +0500

    Stage 5 => Stage 7
    Now for some departments applicant should pass two exams. So for such departments, university.py finds mean results and uses them to admit students.
    Also add functions to get data from txt file, print and save results.

--- python_developer/medium/university.py
+++ python_developer/medium/university.py
@@ -1,16 +1,12 @@
-number_of_students = int(input())
-students = []
-stages = ["plan_a", "plan_b", "plan_c"]
-departments = {"Biotech", "Chemistry", "Engineering", "Mathematics", "Physics"}
-departments_dict = {"Biotech": [], "Chemistry": [], "Engineering": [], "Mathematics": [], "Physics": []}
-exams = {"Biotech": "chem_exam", "Chemistry": "chem_exam", "Engineering": "com_science_exam",
-         "Mathematics": "math_exam", "Physics": "phys_exam"}
-with open("applicants.txt", "r") as f:
-    raw_results = [applicant.rsplit(" ", 7) for applicant in f.read().splitlines()]
-results = {}
-list_of_keys = ["phys_exam", "chem_exam", "math_exam", "com_science_exam", "plan_a", "plan_b", "plan_c"]
-for result in raw_results:
-    results[result[0]] = dict(zip(list_of_keys, result[1:]))
+import statistics
+
+
+def get_results(file):
+    with open(file, "r") as f:
+        raw_results = [applicant.rsplit(" ", 7) for applicant in f.read().splitlines()]
+    list_of_keys = ["phys_exam", "chem_exam", "math_exam", "com_science_exam", "plan_a", "plan_b", "plan_c"]
+    for result in raw_results:
+        results[result[0]] = dict(zip(list_of_keys, result[1:]))
 
 
 def department_sorting(department, applicants, stage):
@@ -18,12 +14,18 @@ def department_sorting(department, applicants, stage):
     temp_departments_dict = {"Biotech": [], "Chemistry": [], "Engineering": [], "Mathematics": [], "Physics": []}
     for applicant in applicants:
         if department in (results[applicant][stage]):
-            temp_departments_dict[department].append((applicant, results[applicant][exams[results[applicant][stage]]]))
+            temp_departments_dict[department].append((applicant, exam_grade(applicant, stage)))
     temp_departments_dict[department].sort(key=lambda x: (-float(x[1]), x[0]))
     temp_departments_dict[department] = temp_departments_dict[department][:places]
     for student in temp_departments_dict[department]:
         departments_dict[department].append(student)
     departments_dict[department].sort(key=lambda x: (-float(x[1]), x[0]))
+
+
+def exam_grade(applicant, stage):
+    applicant_exams = [exam for exam in exams[results[applicant][stage]]]
+    grade = statistics.mean([float(results[applicant][exam]) for exam in applicant_exams])
+    return grade
 
 
 def rotate():
@@ -40,11 +42,33 @@ def rotate():
     stages.pop(0)
 
 
+def print_results():
+    for item in departments_dict.items():
+        print(item[0])
+        for result in item[1]:
+            print(f"{result[0]} {float(result[1])}")
+        print("")
+
+
+def save_results():
+    for item in departments_dict.items():
+        with open(f"{item[0].lower()}.txt", "w") as f:
+            f.writelines([f"{result[0]} {float(result[1])}\n" for result in item[1]])
+
+
+number_of_students = int(input())
+students = []
+stages = ["plan_a", "plan_b", "plan_c"]
+departments = {"Biotech", "Chemistry", "Engineering", "Mathematics", "Physics"}
+departments_dict = {"Biotech": [], "Chemistry": [], "Engineering": [], "Mathematics": [], "Physics": []}
+exams = {"Biotech": {"chem_exam", "phys_exam"},
+         "Chemistry": {"chem_exam"},
+         "Engineering": {"com_science_exam", "math_exam"},
+         "Mathematics": {"math_exam"},
+         "Physics": {"phys_exam", "math_exam"}}
+results = {}
+
+get_results("applicant_list_6.txt")
 while len(departments) != 0 and len(students) != len(results):
     rotate()
-for item in departments_dict.items():
-    print(item[0])
-    for result in item[1]:
-        print(f"{result[0]} {float(result[1])}")
-    print("")
-
+save_results()
