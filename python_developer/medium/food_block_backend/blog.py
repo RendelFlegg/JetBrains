b0db1dfe1f1bc24350139fbc975a71814d309ba0
RendelFlegg <62382457+RendelFlegg@users.noreply.github.com>
2021-07-19 02:25:22 +0500

    Update blog.py
    Stage 1 ==> Stage 2
    Add recipe table and simple way to populate it

--- python_developer/medium/food_block_backend/blog.py
+++ python_developer/medium/food_block_backend/blog.py
@@ -4,24 +4,30 @@ import sqlite3
 data = {"meals": ("breakfast", "brunch", "lunch", "supper"),
         "ingredients": ("milk", "cacao", "strawberry", "blueberry", "blackberry", "sugar"),
         "measures": ("ml", "g", "l", "cup", "tbsp", "tsp", "dsp", "")}
+status = "go"
 conn = sqlite3.connect('food_blog.db')
 c = conn.cursor()
 
-c.execute("""CREATE TABLE meals (
+c.execute("""CREATE TABLE IF NOT EXISTS meals (
             meal_id INT PRIMARY KEY,
             meal_name TEXT NOT NULL UNIQUE
             )""")
 
-c.execute("""CREATE TABLE ingredients (
+c.execute("""CREATE TABLE IF NOT EXISTS ingredients (
             ingredient_id INT PRIMARY KEY,
             ingredient_name TEXT NOT NULL UNIQUE
             )""")
 
-c.execute("""CREATE TABLE measures (
+c.execute("""CREATE TABLE IF NOT EXISTS measures (
             measure_id INT PRIMARY KEY,
             measure_name TEXT UNIQUE
             )""")
 
+c.execute("""CREATE TABLE IF NOT EXISTS recipes (
+            recipe_id INT PRIMARY KEY,
+            recipe_name TEXT NOT NULL,
+            recipe_description TEXT
+            )""")
 conn.commit()
 
 
@@ -29,5 +35,23 @@ for key, value in data.items():
     for idx, element in enumerate(value, 1):
         c.execute(f"INSERT INTO {key} VALUES ('{idx}', '{element}')")
         conn.commit()
+
+
+def populate_recipes():
+    global status
+    name = input('Recipe name: ',)
+    if name == "":
+        status = "break"
+        return status
+    description = input('Recipe description: ',)
+    c.execute(f"INSERT INTO recipes ('recipe_name', 'recipe_description') VALUES ('{name}', '{description}')")
+    conn.commit()
+
+
+print("Pass the empty recipe name to exit.")
+while status == "go":
+    populate_recipes()
+    if status == "break":
+        break
 
 conn.close()
